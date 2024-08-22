41dafe199968621098f668abdd2777ce66d684dc
Aashutosh Singh <ashutosh360a@gmail.com>
2024-08-23 05:06:29 +0530

    commented out the not-ready parts

--- src/main/java/com/Aashu/TacoCloud/TacoOrder.java
+++ src/main/java/com/Aashu/TacoCloud/TacoOrder.java
@@ -1,20 +1,20 @@
-package com.Aashu.TacoCloud;
-import java.util.List;
-import lombok.Data;
-import java.util.ArrayList;
-
-@Data
-public class TacoOrder {
-	  private String deliveryName;
-	  private String deliveryStreet;
-	  private String deliveryCity;
-	  private String deliveryState;
-	  private String deliveryZip;
-	  private String ccNumber;
-	  private String ccExpiration;
-	  private String ccCVV;
-	  private List<Taco> tacos = new ArrayList<>();
-	  public void addTaco(Taco taco) {
-	    this.tacos.add(taco);
-	  }
-	 }
+//package com.Aashu.TacoCloud;
+//import java.util.List;
+//import lombok.Data;
+//import java.util.ArrayList;
+//
+//@Data
+//public class TacoOrder {
+//	  private String deliveryName;
+//	  private String deliveryStreet;
+//	  private String deliveryCity;
+//	  private String deliveryState;
+//	  private String deliveryZip;
+//	  private String ccNumber;
+//	  private String ccExpiration;
+//	  private String ccCVV;
+//	  private List<Taco> tacos = new ArrayList<>();
+//	  public void addTaco(Taco taco) {
+//	    this.tacos.add(taco);
+//	  }
+//	 }
